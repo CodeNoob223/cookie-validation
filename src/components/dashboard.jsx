d03a46bd50691fd87c1fc61fcba74f09ea2fb245
Codenoob223 <vuongdepzai1262003@gmail.com>
2022-11-03 10:29:23 +0700

    commit

---
+++ src/components/dashboard.jsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+    return(
+        <div>
+            <h1>Welcome to dashboard</h1>
+            <p>Random random</p>
+            <button>Logout</button>
+        </div>
+    )
+}
