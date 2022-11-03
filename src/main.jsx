52619a1cee631bb9f381fbb60441d0fe426a7312
Codenoob223 <vuongdepzai1262003@gmail.com>
2022-11-03 10:44:39 +0700

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,17 +5,34 @@ import {
   RouterProvider,
   Route,
 } from "react-router-dom";
+
+//pages
+import LoginPage from "./components/loginpage";
+import DashBoard from "./components/dashboard";
+// import ErrorPage from "./components/errorpage";
+
+//css
 import "./index.css";
 
-const router = createBrowserRouter([
+const myRouter = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <div>Hello world!</div>
   },
-]);
+  {
+    path: "/login",
+    element: <LoginPage />
+  },
+  {
+    path: "/dashboard",
+    element: <DashBoard />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider 
+      router={myRouter}
+    />
   </React.StrictMode>
 );
